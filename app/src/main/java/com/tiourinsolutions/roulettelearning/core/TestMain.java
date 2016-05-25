30c14133089ac0baeff9f58cd2413341b09a94fa
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-24 19:04:52 -0700

    Added quick and dirty initial testing for chain lengths

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/TestMain.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/TestMain.java
@@ -1,10 +1,48 @@
 package com.tiourinsolutions.roulettelearning.core;
 
+import com.tiourinsolutions.roulettelearning.core.roulette.Number;
+import com.tiourinsolutions.roulettelearning.core.roulette.NumberConfiguration;
+import com.tiourinsolutions.roulettelearning.core.roulette.Roulette;
+
 /**
  * @author Maxim Tiourin
  */
 public class TestMain {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        Roulette r = new Roulette(NumberConfiguration.AMERICAN);
+        r.startNewSeries();
+
+        int green = 0, red = 0, black = 0;
+        int maxg = 0, maxr = 0, maxb = 0;
+
+        for (int i = 0; i < 1000; i++) {
+            Number n = r.simulateSpin();
+
+            if (n.getColorId() == Number.NumberColor.Green.getId()) {
+                green += 1;
+                red = 0;
+                black = 0;
+
+                if (green > maxg) maxg = green;
+            }
+            else if (n.getColorId() == Number.NumberColor.Red.getId()) {
+                green = 0;
+                red += 1;
+                black = 0;
+
+                if (red > maxr) maxr = red;
+            }
+            else if (n.getColorId() == Number.NumberColor.Black.getId()) {
+                green = 0;
+                red = 0;
+                black += 1;
+
+                if (black > maxb) maxb = black;
+            }
+        }
+
+        System.out.println("Max Green Chain: " + maxg);
+        System.out.println("Max Red Chain: " + maxr);
+        System.out.println("Max Black Chain: " + maxb);
     }
 }
