593b547cbbb639788153d8ddb1933bfe8c55c109
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-24 18:47:59 -0700

    Added several get methods

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/Number.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/Number.java
@@ -7,16 +7,22 @@ import android.graphics.Color;
  */
 public class Number {
     public enum NumberColor {
-        Red(Color.RED),
-        Black(Color.BLACK),
-        Green(Color.GREEN);
+        Green(0, "Green", Color.GREEN),
+        Red(1, "Red", Color.RED),
+        Black(2, "Black", Color.BLACK);
 
+        private int id;
+        private String name;
         private int color;
 
-        NumberColor(int color) {
+        NumberColor(int id, String name, int color) {
+            this.id = id;
+            this.name = name;
             this.color = color;
         }
 
+        public int getId() { return id; }
+        public String getName() { return name; }
         public int getColor() {
             return color;
         }
@@ -36,5 +42,18 @@ public class Number {
 
     public int getColor() {
         return color.getColor();
+    }
+
+    public int getColorId() {
+        return color.getId();
+    }
+
+    public String getColorName() {
+        return color.getName();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getId() + ", " + getColorName() + ")";
     }
 }
