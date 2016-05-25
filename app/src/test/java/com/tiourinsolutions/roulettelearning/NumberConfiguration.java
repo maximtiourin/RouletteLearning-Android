ee5017b9da55287f32f5d2e7da59666098ca3ad9
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-24 18:47:35 -0700

    Added unit testing for numberconfiguration

---
+++ app/src/test/java/com/tiourinsolutions/roulettelearning/NumberConfiguration.java
@@ -0,0 +1,36 @@
+package com.tiourinsolutions.roulettelearning;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class NumberConfiguration {
+    private com.tiourinsolutions.roulettelearning.core.roulette.NumberConfiguration configAmerican;
+    private com.tiourinsolutions.roulettelearning.core.roulette.NumberConfiguration configTester;
+
+    public NumberConfiguration() {
+        configAmerican = com.tiourinsolutions.roulettelearning.core.roulette.NumberConfiguration.AMERICAN;
+        configTester = configAmerican;
+    }
+
+    @Test
+    public void AmericanConfig() throws Exception {
+        assertEquals(configAmerican.getConfigLength(), 38);
+        assertEquals(configAmerican.getConfigGreenCount(), 2);
+        assertEquals(configAmerican.getConfigRedCount(), 18);
+        assertEquals(configAmerican.getConfigBlackCount(), 18);
+        assertEquals(configAmerican.getNumberAtIndexOffset(0).getId(), "00");
+        assertEquals(configAmerican.getNumberAtIndexOffset(19).getId(), "0");
+    }
+
+    @Test
+    public void getIndexAtIndexOffset() throws Exception {
+        assertEquals(configTester.getIndexAtIndexOffset(0), 0);
+        assertEquals(configTester.getIndexAtIndexOffset(37), 37);
+        assertEquals(configTester.getIndexAtIndexOffset(38), 0);
+        assertEquals(configTester.getIndexAtIndexOffset(16), 16);
+    }
+}
