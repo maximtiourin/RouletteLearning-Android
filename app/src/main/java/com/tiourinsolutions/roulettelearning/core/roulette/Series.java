2c3dfd1a6dc0ead670107e0849d05cc656bae3b3
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-27 23:47:06 -0700

    Modified series to be a container for resultListeners for use in statistics, as well as removed the Result type, so that Number is passed directly.

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/Series.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/Series.java
@@ -1,24 +1,74 @@
 package com.tiourinsolutions.roulettelearning.core.roulette;
 
+import com.tiourinsolutions.roulettelearning.core.roulette.statistics.ColorChainFrequency;
+import com.tiourinsolutions.roulettelearning.core.roulette.statistics.ColorFrequency;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A series is a collection of results of consecutive "spins" of a roulette table.
+ * A series is a collection of results of consecutive "spins" of a roulette table, as well as the
+ * container for all statistics pertaining to the series.
  * @author Maxim Tiourin
  */
 public class Series {
-    protected ArrayList<Result> series;
+    protected ArrayList<Number> series;
+    protected ArrayList<ResultListener> resultListeners;
 
     public Series() {
-        series = new ArrayList<Result>();
+        series = new ArrayList<Number>();
+        resultListeners = new ArrayList<ResultListener>();
+
+        initResultListeners();
     }
 
-    public void addResult(Result result) {
+    private void initResultListeners() {
+        //Green Color Frequency
+        addResultListener(new ColorFrequency(ColorFrequency.ID_FREQUENCY_COLOR_GREEN, Number.NumberColor.Green.getId()));
+        //Red Color Frequency
+        addResultListener(new ColorFrequency(ColorFrequency.ID_FREQUENCY_COLOR_RED, Number.NumberColor.Red.getId()));
+        //Black Color Frequency
+        addResultListener(new ColorFrequency(ColorFrequency.ID_FREQUENCY_COLOR_BLACK, Number.NumberColor.Black.getId()));
+
+        //Green Color Chain Frequency
+        addResultListener(new ColorChainFrequency(ColorChainFrequency.ID_FREQUENCY_CHAIN_COLOR_GREEN, Number.NumberColor.Green.getId()));
+        //Red Color Chain Frequency
+        addResultListener(new ColorChainFrequency(ColorChainFrequency.ID_FREQUENCY_CHAIN_COLOR_RED, Number.NumberColor.Red.getId()));
+        //Black Color Chain Frequency
+        addResultListener(new ColorChainFrequency(ColorChainFrequency.ID_FREQUENCY_CHAIN_COLOR_BLACK, Number.NumberColor.Black.getId()));
+    }
+
+    public void addResult(Number result) {
         series.add(result);
+
+        notifyResultListeners(new ResultEvent(result));
     }
 
-    public List<Result> getResults() {
+    public List<Number> getResults() {
         return series;
+    }
+
+    public void addResultListener(ResultListener rl) {
+        resultListeners.add(rl);
+    }
+
+    private void notifyResultListeners(ResultEvent re) {
+        for (ResultListener rl : resultListeners) {
+            rl.notify(re);
+        }
+    }
+
+    public ResultListener getResultListener(String id) {
+        for (ResultListener rl : resultListeners) {
+            if (rl.getId().equals(id)) {
+                return rl;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean removeResultListener(ResultListener rl) {
+        return resultListeners.remove(rl);
     }
 }
