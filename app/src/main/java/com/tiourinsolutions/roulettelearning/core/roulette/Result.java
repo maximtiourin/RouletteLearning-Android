9a9f67e3452311791138e13ff1bc492074aadb05
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-27 23:54:41 -0700

    sync modification

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/Result.java
+++
@@ -1,9 +0,0 @@
-package com.tiourinsolutions.roulettelearning.core.roulette;
-
-/**
- * A result is a single state of the roulette table after a spin in the series. It stores the "spin"
- * result, as well as a collection of all of the probabilities leading up to the spin at that time.
- * @author Maxim Tiourin
- */
-public class Result {
-}
