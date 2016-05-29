b997df20b3b7b7a003f8bafa285ede7f45cd6ee5
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-28 20:18:18 -0700

    Added test output of chain length frequency as well as chainLengthDistances

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/TestMain.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/TestMain.java
@@ -13,15 +13,20 @@ import com.tiourinsolutions.roulettelearning.core.roulette.statistics.ColorFrequ
 public class TestMain {
     public static void main(String args[]) {
         Roulette r = new Roulette(NumberConfiguration.AMERICAN);
-        r.startNewSeries();
-        Series series = r.getCurrentSeries();
 
+        r.startNewSeries();
+
+        Series series = r.getCurrentSeries();
         int green = 0, red = 0, black = 0;
         int spins = 5000;
+        int formatOffset = (spins + "").length();
 
         for (int i = 0; i < spins; i++) {
             Number n = r.simulateSpin();
+            //System.out.println(n.toString());
         }
+
+        r.concludeSeries();
 
         System.out.println("--------------------------");
         System.out.println("Series (" + spins + " spins)");
@@ -40,10 +45,19 @@ public class TestMain {
         ColorChainFrequency redccf = (ColorChainFrequency) series.getResultListener(ColorChainFrequency.ID_FREQUENCY_CHAIN_COLOR_RED);
         ColorChainFrequency blackccf = (ColorChainFrequency) series.getResultListener(ColorChainFrequency.ID_FREQUENCY_CHAIN_COLOR_BLACK);
 
-        if (greenccf != null) System.out.println("Max Green Chain: " + greenccf.getHighestChainLength());
-        if (redccf != null) System.out.println("Max Red Chain: " + redccf.getHighestChainLength());
-        if (blackccf != null) System.out.println("Max Black Chain: " + blackccf.getHighestChainLength());
+        if (greenccf != null) System.out.println("Max Green Chain: " + greenccf.getMaxChainLength());
+        if (redccf != null) System.out.println("Max Red Chain: " + redccf.getMaxChainLength());
+        if (blackccf != null) System.out.println("Max Black Chain: " + blackccf.getMaxChainLength());
 
-        r.concludeSeries();
+        if (redccf != null) {
+            for (int i = 1; i <= redccf.getMaxChainLength(); i++) {
+                int n = redccf.getChainLengthCount(i);
+
+                System.out.printf("Amount of Red x %2d Chain: %" + formatOffset +
+                        "d  ::  (Min: %" + formatOffset + "d, Avg: %" + (formatOffset + 7) +
+                        ".2f, Max: %" + formatOffset + "d) %s\n",
+                        i, n, redccf.getMinChainLengthDistance(i), redccf.getAvgChainLengthDistance(i), redccf.getMaxChainLengthDistance(i), redccf.getChainLengthDistances(i));
+            }
+        }
     }
 }
