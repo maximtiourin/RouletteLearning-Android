892b25f46ea6d18f27dc6db5e1b3ee00d7379a07
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-05-30 22:20:43 -0700

    Modified ChainFrequency to track an unknown amount of chain counts rather than low fixed upper limit (which was only viable for tracking color chains). This adds time and space overhead, but allows for far more robust chain frequency tracking for an arbitrary collection of events.

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/statistics/ChainFrequency.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/statistics/ChainFrequency.java
@@ -1,6 +1,5 @@
 package com.tiourinsolutions.roulettelearning.core.roulette.statistics;
 
-import com.tiourinsolutions.roulettelearning.core.roulette.Number;
 import com.tiourinsolutions.roulettelearning.core.roulette.ResultEvent;
 
 import java.util.ArrayList;
@@ -10,47 +9,144 @@ import java.util.List;
  * @author Maxim Tiourin
  */
 public abstract class ChainFrequency extends Frequency {
-    public static final int MAX_TABLE_COUNT = 256; //Seeing a chain length of 256 is highly improbable
     protected int maxCount;
-    protected int countTable[];
-    protected int distance[]; //The current tracking distance for each chain length
+    protected int overallDistance; //A distance counter that just tracks the amount of results.
+    protected ArrayList<Integer> countTable;
+    protected ArrayList<Integer> distance; //The current tracking distance for each chain length
     protected ArrayList<ArrayList<Integer>> distanceTable; //The 2 dimensional array list tracking all of the distances of a chain length
 
     public ChainFrequency(String id) {
         super(id);
 
         maxCount = 0;
+        overallDistance = 0;
 
-        countTable = new int[MAX_TABLE_COUNT];
-        distance = new int[MAX_TABLE_COUNT];
+        countTable = new ArrayList<Integer>();
+        distance = new ArrayList<Integer>();
         distanceTable = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < MAX_TABLE_COUNT; i++) {
-            distanceTable.add(new ArrayList<Integer>());
-        }
     }
 
     public void notify(ResultEvent re) {
         super.notify(re);
 
         if (!evaluateResultEvent(re) && count != 0) {
-            if (count < MAX_TABLE_COUNT) {
-                //Track Chain Length Frequency
-                countTable[count]++;
+            //Track Chain Length Frequency
+            incrementCountTable(count);
 
-                //Track Distance
-                distanceTable.get(count).add(distance[count] - count);
-                distance[count] = 0;
-            }
-            else {
-                //Will most likely never occur
-                System.out.println("A chain length greater than length " + (MAX_TABLE_COUNT - 1) + " has occurred for " + id + " : " + count);
-            }
+            //Track Distance
+            addToDistanceTable(count);
+            setDistance(count, 0);
+
             if (count > maxCount) maxCount = count; //Determine highest chain length
             count = 0;
         }
 
-        for (int i = 1; i < MAX_TABLE_COUNT; i++) {
-            distance[i]++;
+        for (int i = 1; i < distance.size(); i++) {
+            incrementDistance(i);
+        }
+
+        overallDistance++;
+    }
+
+    protected void incrementCountTable(int index) {
+        allocateCountTable(index);
+
+        countTable.set(index, countTable.get(index) + 1);
+    }
+
+    protected void incrementDistance(int index) {
+        allocateDistance(index);
+
+        distance.set(index, distance.get(index) + 1);
+    }
+
+    protected void addToDistanceTable(int count) {
+        allocateDistanceTable(count);
+
+        distanceTable.get(count).add(getDistance(count) - count);
+    }
+
+    protected int getCountTable(int index) {
+        allocateCountTable(index);
+
+        return countTable.get(index);
+    }
+
+    protected int getDistance(int index) {
+        allocateDistance(index);
+
+        return distance.get(index);
+    }
+
+    protected void setDistance(int index, int value) {
+        allocateDistance(index);
+
+        distance.set(index, value);
+    }
+
+    protected List<Integer> getDistanceTable(int index) {
+        allocateDistanceTable(index);
+
+        return distanceTable.get(index);
+    }
+
+    protected boolean allocateCountTable(int count) {
+        if (count > countTable.size() - 1) {
+            ArrayList<Integer> newCountTable = new ArrayList<Integer>(count + 1);
+            for (int i = 0; i < count + 1; i++) {
+                if (i < countTable.size()) {
+                    newCountTable.add(countTable.get(i));
+                }
+                else {
+                    newCountTable.add(0);
+                }
+            }
+            countTable = newCountTable;
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    protected boolean allocateDistance(int count) {
+        if (count > distance.size() - 1) {
+            ArrayList<Integer> newDistance = new ArrayList<Integer>(count + 1);
+            for (int i = 0; i < count + 1; i++) {
+                if (i < distance.size()) {
+                    newDistance.add(distance.get(i));
+                }
+                else {
+                    newDistance.add(overallDistance);
+                }
+            }
+            distance = newDistance;
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    protected boolean allocateDistanceTable(int count) {
+        if (count > distanceTable.size() - 1) {
+            ArrayList<ArrayList<Integer>> newDistanceTable = new ArrayList<ArrayList<Integer>>(count + 1);
+            for (int i = 0; i < count + 1; i++) {
+                if (i < distanceTable.size()) {
+                    newDistanceTable.add(distanceTable.get(i));
+                }
+                else {
+                    newDistanceTable.add(new ArrayList<Integer>());
+                }
+            }
+            distanceTable = newDistanceTable;
+
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
@@ -60,24 +156,24 @@ public abstract class ChainFrequency extends Frequency {
 
     /**
      * Returns how often the 'n' chain length occurred
-     * Returns -1 if 'n' chain length is out of bounds of [0, MAX_TABLE_COUNT)
+     * Returns 0 if the chain length hasn't occurred, even if it isn't allocated
      */
     public int getChainLengthCount(int n) {
-        if (n >= 0 && n <= countTable.length) {
-            return countTable[n];
+        if (n >= 0 && n < countTable.size()) {
+            return getCountTable(n);
         }
         else {
-            return -1;
+            return 0;
         }
     }
 
     /**
      * Returns the maximum occurrence distance for an 'n' chain length
-     * Returns -1 if 'n' chain length is out of bounds of [0, MAX_TABLE_COUNT), or hasn't occurred.
+     * Returns -1 if 'n' chain length hasn't occurred
      */
     public int getMaxChainLengthDistance(int n) {
-        if (n >= 0 && n <= distanceTable.size()) {
-            ArrayList<Integer> table = distanceTable.get(n);
+        if (n >= 0 && n < distanceTable.size()) {
+            ArrayList<Integer> table = (ArrayList<Integer>) getDistanceTable(n);
 
             if (table.size() > 0) {
                 int max = 0;
@@ -99,11 +195,11 @@ public abstract class ChainFrequency extends Frequency {
 
     /**
      * Returns the minimum occurrence distance for an 'n' chain length
-     * Returns -1 if 'n' chain length is out of bounds of [0, MAX_TABLE_COUNT), or hasn't occurred.
+     * Returns -1 if 'n' chain length hasn't occurred
      */
     public int getMinChainLengthDistance(int n) {
-        if (n >= 0 && n <= distanceTable.size()) {
-            ArrayList<Integer> table = distanceTable.get(n);
+        if (n >= 0 && n < distanceTable.size()) {
+            ArrayList<Integer> table = (ArrayList<Integer>) getDistanceTable(n);
 
             if (table.size() > 0) {
                 int min = Integer.MAX_VALUE;
@@ -115,6 +211,7 @@ public abstract class ChainFrequency extends Frequency {
                 return min;
             }
             else {
+                System.out.println("n = " + n + ", distanceTable(n) return a size 0 table");
                 return -1;
             }
         }
@@ -125,12 +222,11 @@ public abstract class ChainFrequency extends Frequency {
 
     /**
      * Returns the average occurrence distance for an 'n' chain length
-     * Returns NaN if 'n' chain length is out of bounds of [0, MAX_TABLE_COUNT)
      * Returns Positive.Infinity if chain length hasn't occurred.
      */
     public float getAvgChainLengthDistance(int n) {
-        if (n >= 0 && n <= distanceTable.size()) {
-            ArrayList<Integer> table = distanceTable.get(n);
+        if (n >= 0 && n < distanceTable.size()) {
+            ArrayList<Integer> table = (ArrayList<Integer>) getDistanceTable(n);
 
             if (table.size() > 0) {
                 int sum = 0;
@@ -146,7 +242,7 @@ public abstract class ChainFrequency extends Frequency {
             }
         }
         else {
-            return Float.NaN;
+            return Float.POSITIVE_INFINITY;
         }
     }
 
@@ -155,14 +251,14 @@ public abstract class ChainFrequency extends Frequency {
      * A chain length occurrence distance is the amount of 'other' results between
      * two chains of the same length or between one chain length and the start of the series,
      * IE: abbaabb, the first occurrence distance of 'bb' is 1, the second occurrence distance of 'bb' is 2.
-     * Returns null if 'n' chain length is out of bounds of [0, MAX_TABLE_COUNT)
+     * Returns empty list if 'n' chain length hasn't been encountered
      */
     public List<Integer> getChainLengthDistances(int n) {
-        if (n >= 0 && n <= distanceTable.size()) {
-            return distanceTable.get(n);
+        if (n >= 0 && n < distanceTable.size()) {
+            return getDistanceTable(n);
         }
         else {
-            return null;
+            return new ArrayList<Integer>();
         }
     }
 }
