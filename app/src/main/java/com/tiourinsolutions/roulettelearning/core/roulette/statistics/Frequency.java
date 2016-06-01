d6b3c0d91687f6cfabeb55d1171c60b3727e0e54
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-06-01 09:39:23 -0700

    Added more const id strings

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/statistics/Frequency.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/statistics/Frequency.java
@@ -14,9 +14,23 @@ public abstract class Frequency implements ResultListener {
     public static final String ID_FREQUENCY_GREEN = "frequency_green";
     public static final String ID_FREQUENCY_RED = "frequency_red";
     public static final String ID_FREQUENCY_BLACK = "frequency_black";
+    public static final String ID_FREQUENCY_EVEN = "frequency_even";
+    public static final String ID_FREQUENCY_ODD = "frequency_odd";
+    public static final String ID_FREQUENCY_1TO18 = "frequency_1to18";
+    public static final String ID_FREQUENCY_19TO36 = "frequency_19to36";
+    public static final String ID_FREQUENCY_COLUMN1 = "frequency_column1";
+    public static final String ID_FREQUENCY_COLUMN2 = "frequency_column2";
+    public static final String ID_FREQUENCY_COLUMN3 = "frequency_column3";
     public static final String ID_FREQUENCY_NOT_GREEN = "frequency_not_green";
     public static final String ID_FREQUENCY_NOT_RED = "frequency_not_red";
     public static final String ID_FREQUENCY_NOT_BLACK = "frequency_not_black";
+    public static final String ID_FREQUENCY_NOT_EVEN = "frequency_not_even";
+    public static final String ID_FREQUENCY_NOT_ODD = "frequency_not_odd";
+    public static final String ID_FREQUENCY_NOT_1TO18 = "frequency_not_1to18";
+    public static final String ID_FREQUENCY_NOT_19TO36 = "frequency_not_19to36";
+    public static final String ID_FREQUENCY_NOT_COLUMN1 = "frequency_not_column1";
+    public static final String ID_FREQUENCY_NOT_COLUMN2 = "frequency_not_column2";
+    public static final String ID_FREQUENCY_NOT_COLUMN3 = "frequency_not_column3";
 
     protected String id;
     protected String eventName;
