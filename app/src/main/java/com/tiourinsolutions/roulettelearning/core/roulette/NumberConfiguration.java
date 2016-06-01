e759e9dc07ebb3a39cf636d0ed2a3e467e17186e
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-06-01 06:35:27 -0700

    Accounted for the change in Number() constructor signature.

--- app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/NumberConfiguration.java
+++ app/src/main/java/com/tiourinsolutions/roulettelearning/core/roulette/NumberConfiguration.java
@@ -2,6 +2,8 @@ package com.tiourinsolutions.roulettelearning.core.roulette;
 
 import java.lang.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Maxim Tiourin
@@ -14,44 +16,44 @@ public abstract class NumberConfiguration {
 
         @Override
         protected void initNumbers() {
-            numbers.add(new Number("00", Number.NumberColor.Green));
-            numbers.add(new Number("27", Number.NumberColor.Red));
-            numbers.add(new Number("10", Number.NumberColor.Black));
-            numbers.add(new Number("25", Number.NumberColor.Red));
-            numbers.add(new Number("29", Number.NumberColor.Black));
-            numbers.add(new Number("12", Number.NumberColor.Red));
-            numbers.add(new Number("8", Number.NumberColor.Black));
-            numbers.add(new Number("19", Number.NumberColor.Red));
-            numbers.add(new Number("31", Number.NumberColor.Black));
-            numbers.add(new Number("18", Number.NumberColor.Red));
-            numbers.add(new Number("6", Number.NumberColor.Black));
-            numbers.add(new Number("21", Number.NumberColor.Red));
-            numbers.add(new Number("33", Number.NumberColor.Black));
-            numbers.add(new Number("16", Number.NumberColor.Red));
-            numbers.add(new Number("4", Number.NumberColor.Black));
-            numbers.add(new Number("23", Number.NumberColor.Red));
-            numbers.add(new Number("35", Number.NumberColor.Black));
-            numbers.add(new Number("14", Number.NumberColor.Red));
-            numbers.add(new Number("2", Number.NumberColor.Black));
-            numbers.add(new Number("0", Number.NumberColor.Green));
-            numbers.add(new Number("28", Number.NumberColor.Black));
-            numbers.add(new Number("9", Number.NumberColor.Red));
-            numbers.add(new Number("26", Number.NumberColor.Black));
-            numbers.add(new Number("30", Number.NumberColor.Red));
-            numbers.add(new Number("11", Number.NumberColor.Black));
-            numbers.add(new Number("7", Number.NumberColor.Red));
-            numbers.add(new Number("20", Number.NumberColor.Black));
-            numbers.add(new Number("32", Number.NumberColor.Red));
-            numbers.add(new Number("17", Number.NumberColor.Black));
-            numbers.add(new Number("5", Number.NumberColor.Red));
-            numbers.add(new Number("22", Number.NumberColor.Black));
-            numbers.add(new Number("34", Number.NumberColor.Red));
-            numbers.add(new Number("15", Number.NumberColor.Black));
-            numbers.add(new Number("3", Number.NumberColor.Red));
-            numbers.add(new Number("24", Number.NumberColor.Black));
-            numbers.add(new Number("36", Number.NumberColor.Red));
-            numbers.add(new Number("13", Number.NumberColor.Black));
-            numbers.add(new Number("1", Number.NumberColor.Red));
+            numbers.add(new Number("00", 0, Number.NumberColor.Green));
+            numbers.add(new Number("27", 27, Number.NumberColor.Red));
+            numbers.add(new Number("10", 10, Number.NumberColor.Black));
+            numbers.add(new Number("25", 25, Number.NumberColor.Red));
+            numbers.add(new Number("29", 29, Number.NumberColor.Black));
+            numbers.add(new Number("12", 12, Number.NumberColor.Red));
+            numbers.add(new Number("8", 8, Number.NumberColor.Black));
+            numbers.add(new Number("19", 19, Number.NumberColor.Red));
+            numbers.add(new Number("31", 31, Number.NumberColor.Black));
+            numbers.add(new Number("18", 18, Number.NumberColor.Red));
+            numbers.add(new Number("6", 6, Number.NumberColor.Black));
+            numbers.add(new Number("21", 21, Number.NumberColor.Red));
+            numbers.add(new Number("33", 33, Number.NumberColor.Black));
+            numbers.add(new Number("16", 16, Number.NumberColor.Red));
+            numbers.add(new Number("4", 4, Number.NumberColor.Black));
+            numbers.add(new Number("23", 23, Number.NumberColor.Red));
+            numbers.add(new Number("35", 35, Number.NumberColor.Black));
+            numbers.add(new Number("14", 14, Number.NumberColor.Red));
+            numbers.add(new Number("2", 2, Number.NumberColor.Black));
+            numbers.add(new Number("0", 0, Number.NumberColor.Green));
+            numbers.add(new Number("28", 28, Number.NumberColor.Black));
+            numbers.add(new Number("9", 9, Number.NumberColor.Red));
+            numbers.add(new Number("26", 26, Number.NumberColor.Black));
+            numbers.add(new Number("30", 30, Number.NumberColor.Red));
+            numbers.add(new Number("11", 11, Number.NumberColor.Black));
+            numbers.add(new Number("7", 7, Number.NumberColor.Red));
+            numbers.add(new Number("20", 20, Number.NumberColor.Black));
+            numbers.add(new Number("32", 32, Number.NumberColor.Red));
+            numbers.add(new Number("17", 17, Number.NumberColor.Black));
+            numbers.add(new Number("5", 5, Number.NumberColor.Red));
+            numbers.add(new Number("22", 22, Number.NumberColor.Black));
+            numbers.add(new Number("34", 34, Number.NumberColor.Red));
+            numbers.add(new Number("15", 15, Number.NumberColor.Black));
+            numbers.add(new Number("3", 3, Number.NumberColor.Red));
+            numbers.add(new Number("24", 24, Number.NumberColor.Black));
+            numbers.add(new Number("36", 36, Number.NumberColor.Red));
+            numbers.add(new Number("13", 13, Number.NumberColor.Black));
+            numbers.add(new Number("1", 1, Number.NumberColor.Red));
         }
     }
 
@@ -123,5 +125,9 @@ public abstract class NumberConfiguration {
             }
         }
         return c;
+    }
+
+    public List<Number> getNumberList() {
+        return numbers;
     }
 }
